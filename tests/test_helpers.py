c3cfb554b5c25b727a69b77081b4de066b75128f
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2015-10-24 21:52:48 -0400

    Add helper to calculate clockwise arc length between two points
    Add a helper to calculate the clockwise arc length between two points,
    where the arc is centered on a center c with radius r.

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -13,11 +13,19 @@ v8 = (1, 1)
 
 
 def test_get_angle():
-    assert (helpers.get_angle(v1) * 180 / np.pi) == 0.0
-    assert (helpers.get_angle(v2) * 180 / np.pi) == -45.0
-    assert (helpers.get_angle(v3) * 180 / np.pi) == -90.0
-    assert (helpers.get_angle(v4) * 180 / np.pi) == - 135.0
-    assert (helpers.get_angle(v5) * 180 / np.pi) == -180.0
-    assert (helpers.get_angle(v6) * 180 / np.pi) == -225.0
-    assert (helpers.get_angle(v7) * 180 / np.pi) == -270.0
-    assert (helpers.get_angle(v8) * 180 / np.pi) == -315.0
+    assert np.abs((helpers.get_angle(v1) * 180 / np.pi) - 0.0) < 0.00001
+    assert np.abs((helpers.get_angle(v2) * 180 / np.pi) - -45.0) < 0.00001
+    assert np.abs((helpers.get_angle(v3) * 180 / np.pi) - -90.0) < 0.00001
+    assert np.abs((helpers.get_angle(v4) * 180 / np.pi) - - 135.0) < 0.00001
+    assert np.abs((helpers.get_angle(v5) * 180 / np.pi) - -180.0) < 0.00001
+    assert np.abs((helpers.get_angle(v6) * 180 / np.pi) - -225.0) < 0.00001
+    assert np.abs((helpers.get_angle(v7) * 180 / np.pi) - -270.0) < 0.00001
+    assert np.abs((helpers.get_angle(v8) * 180 / np.pi) - -315.0) < 0.00001
+
+
+def test_get_clockwise_arc_length():
+    assert np.abs((helpers.get_clockwise_arc_length((1, 0), (0, 1), center=(1, 1), radius=1)) - (np.pi / 2)) < 0.00001
+    assert np.abs((helpers.get_clockwise_arc_length((0, 1), (1, 0), center=(1, 1), radius=1)) - (3 * np.pi / 2)) < 0.00001
+    assert np.abs((helpers.get_clockwise_arc_length((1, 2), (1, 0), center=(1, 1), radius=1)) - (np.pi)) < 0.00001
+    assert np.abs((helpers.get_clockwise_arc_length((1, 0), (1, 2), center=(1, 1), radius=1)) - (np.pi)) < 0.00001
+    assert np.abs((helpers.get_clockwise_arc_length((1, 0), (1, 0), center=(1, 1), radius=1)) - (2 * np.pi)) < 0.00001
