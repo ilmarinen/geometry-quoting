d2666de4cefd091c96133c3a8361f9eea6e06d6d
fernandes.praphat@gmail.com <fernandes.praphat@gmail.com>
2015-10-25 20:10:10 -0400

    Add the word 'dollars' to the output
    Add the word dollars to the end of the output.

--- calculate.py
+++ calculate.py
@@ -8,7 +8,7 @@ def quote(filepath=('f', '', 'Path to profile representation file'),
           optimize_rectangle=('', False, 'Optimize for the smallest rectangular\
             piece of stock which can contain the profile')):
     data = load_file(filepath, optimize=optimize_rectangle)
-    print '{0:.2f}'.format(make_quote(data))
+    print '{0:.2f} dollars'.format(make_quote(data))
 
 
 if __name__ == '__main__':
